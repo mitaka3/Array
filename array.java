7c5262e4c131a12a81df029b8f71c1a43652d51e
mitaka3 <justinbituin1207@gmail.com>
2023-10-22 02:45:01 +0800

    initial commit

---
+++ array.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * @author user
+ */
+import javax.swing.JOptionPane;
+public class Array {
+    public static void main(String[] args)
+    {
+        int x;
+        String[] vowel = {"a","e","i","o","u"};
+        String vow;
+        boolean validVowel = false;
+        
+        vow = JOptionPane.showInputDialog(null, "Enter a vowel:");
+        for (x=0 ; x< vowel.length; ++x)
+            if(vow.equals(vowel[x]))
+                    validVowel = true;
+        
+        if (validVowel)
+            JOptionPane.showMessageDialog(null,"The vowel that you entered is: " + vow);
+        
+        else
+            JOptionPane.showMessageDialog(null, "Invalid character used");
+            
+     
+    }
+}
